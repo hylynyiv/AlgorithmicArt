4e7132ab64f21a95de4e321f4659aa2ecf2fb43b
hylynyiv <info@hylynyiv.com>
2023-11-08 14:51:44 +0100

    Adding Boilerplate.py

--- Boilerplate.py
+++ Boilerplate.py
@@ -1,14 +1,22 @@
 import pygame
+from pygame.locals import QUIT
+from sys import exit
 
+# Initialise Pygame
 pygame.init()
 
+# Instantiate clock object
+clock = pygame.time.Clock()
+
+# Initialise screen
 width = 1000
 height = 1000
-
 screen = pygame.display.set_mode((width,height))
 
+# Main game loop
 while True:
     for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+        if e.type == QUIT:
+            pygame.quit() # uninitialise all Pygame modules, release all resources
+            exit()        # exit program execution and Python interpreter
+    clock.tick(30)        # limit framerate to 30 frames per second
